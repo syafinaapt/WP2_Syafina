2d21fa9d7dd0d4fedce34dacf33795df0d5bc05d
syafinaapt <145653044+syafinaapt@users.noreply.github.com>
2023-11-02 12:31:13 +0700

    Add files via upload

---
+++ views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?><h/1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Syafina</li>
+        <li>Nama Belakang : Putri Roynaldi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Perumahan Lembah Hijau Jalan Melati 5 No. 02</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Workout</li>
+        <li>Badminton</li>
+    </ul>
+</section>
---
+++ views/v_index.php
@@ -0,0 +1,19 @@
+<head>
+<link rel="stylesheet" type="text/css"
+href="http://localhost/pustaka-booking/assets/stylebuku.css">
+      
+        <section>
+            <h1><?php echo $judul ?></h1>
+            <p align='justify'>Pada pengertian codeigniter di atas tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih jauh dalam belajar codeigniter.</p>
+            <p>MVC adalah teknik atau konsep yang memisahkan komponen utama menjadi tiga komponen yaitu model, view dan controller.</p>
+
+                <ol type="a">
+                <li>Model</li>
+<p align='justify'>Model adalah kelas yang merepresentasikan atau memodelkan tipe data yang akan digunakan oleh aplikasi.</p>
+<p align='justify'>Sebagai catatan, Semua model harus disimpan di dalam folder application\models</p>
+<li>View</li>
+<p align='justify'>View merupakan bagian yang menangani halaman user interface atau halaman yang muncul pada user(pada browser).</p>
+<li>Controller</li>
+<p align='justify'>Controller merupakan kumpulan intruksi aksi yang menghubungkan model dan view, jadi user tidak akan berhubungan dengan model secara langsung, intinya data yang tersimpan di database (model) di ambil oleh controller dan kemudian controller pula yang menampilkan nya ke view. Jadi controller lah yang mengolah intruksi.</p>
+<p align='justify'>Dari penjelasan tentang model view dan controller di atas dapat di simpulkan bahwa controller sebagai penghubung view dan model.</p>
+    </section>
